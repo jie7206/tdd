d3c7d8ef1f8b7990415eefe8d5b9901b1a140cc7
jie7206 <1524556052@qq.com>
2019-09-21 12:44:18 +0800

    new specify '名称的长度不能超过50个字元'

---
+++ app/models/project.rb
@@ -0,0 +1,5 @@
+class Project < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,20 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "项目(Project)" do
+RSpec.describe '项目(Project)' do
 
-  specify "必须要有名字和创始时间才能成功创建" do
-    project = Project.new
+  let(:project) { Project.new }
+
+  specify '必须要有名称才能成功创建' do
     expect(project).to_not be_valid
-    project.name = "测试用项目1"
-    project.created_at = Time.now
+    project.name = '测试用项目1'
     expect(project).to be_valid
+  end
+
+  specify '名称的长度不能超过50个字元' do
+    project.name = 'a'*50
+    expect(project).to be_valid
+    project.name = 'a'*51
+    expect(project).to_not be_valid
   end
 
 end
